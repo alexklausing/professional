e91343ff8c6cd7659bea273884c0134ed110191d
Alex <63033560+bkper087@users.noreply.github.com>
2021-01-02 17:25:53 -0600

    1

---
+++ assets/navigation.php
@@ -0,0 +1,14 @@
+<head>
+<link rel="stylesheet" href="css/navigation.css" type="text/css">
+</head>
+   
+
+<header>
+    
+<nav>
+<div class="logo-box">
+    <p class="logo-text">Alex <span>Klausing</span></p>
+</div>
+</nav>
+    
+</header>
---
+++ index.php
@@ -0,0 +1,171 @@
+<?php
+require 'assets/header.php';
+?>
+
+<!--Page Specific Title and Description Information for SEO-->
+
+<head>
+    <title>Hire Alex Klausing in 2021! | Nonprofit | Outdoor Recreation</title>
+    <meta name="description" content="A professional portfolio of Alex Klausing, nonprofit, camping, and outdoor recreation programs professional with ten years of experiance.">
+    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <script src="js/modal.js"></script>
+
+</head>
+
+<!--Navigation Bar-->
+<nav>
+    <?php
+    require 'assets/navigation.php'
+    ?>
+</nav>
+
+<body>
+    <section class="cover">
+        <div class="hero-text">
+            <p class="headline1">Alex Klausing</p>
+            <p class="tagline1">Nonprofit Leadership | Camp Administration | Adventure Programming</p>
+        </div>
+    </section>
+  
+<section class="highlight-container">
+<div class="highlight-box">
+    <p><label class="btn callout1" for="modal-1">Over $500,000 Raised</label></p></div>
+<div class="highlight-box">
+  
+  
+ 
+   
+<input class="modal-state" id="modal-1" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-1"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-1"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>
+   
+<p><label class="btn callout1" for="modal-2">Over $500,000 Raised</label></p></div>
+<div class="highlight-box">
+   
+<input class="modal-state" id="modal-2" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-2"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-2"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>   
+   
+    <p><label class="btn callout1" for="modal-3">Over $500,000 Raised</label></p></div>
+<div class="highlight-box">
+   
+   <input class="modal-state" id="modal-3" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-3"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-3"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>
+   
+    <p><label class="btn callout1" for="modal-4">Over $500,000 Raised</label></p></div>
+    
+       <input class="modal-state" id="modal-4" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-4"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-4"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>
+      
+<div class="highlight-box">
+    <p><label class="btn callout1" for="modal-5">Over $500,000 Raised</label></p></div>
+    
+       <input class="modal-state" id="modal-5" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-5"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-5"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>
+       
+<div class="highlight-box">
+    <p><label class="btn callout1" for="modal-6">Over $500,000 Raised</label></p></div>
+
+   <input class="modal-state" id="modal-6" type="checkbox" />
+<div class="modal">
+  <label class="modal__bg" for="modal-6"></label>
+  <div class="modal__inner">
+    <label class="modal__close" for="modal-6"></label>
+    <h2>Fundraising Experiance</h2>
+    <p>With nearly 10 years of professional fundraising experiance, Alex has been exposed to almost every type of development process. Annual campaigns are where most of Alex's experiance lies.</p>
+  </div>
+</div>
+
+
+<section class="endorsement-container">
+    <div class="endorsement-box">
+       <div class="endorsement-photo">
+           <img src="images/alex1.jpg" alt="Endorsed by Picture">
+       </div>
+        <p id="para1">Alex is Great! You should hire him.</p>
+    </div>
+</section>
+
+<div class="section-break">
+<p id="headline3">Full-Time Employers</p>
+</div>
+<section class="trustedby-container">
+    <div class="trustedby-box">
+        <img src="images/Lincoln_Heritage_Stack_4C.jpg" alt="Northern Tier High Adventure, BSA Logo">
+        <p id="para1"><span>Lincoln Heritage Council</span><br>Boy Scouts of America <br>Louisville, Kentucky <br></p>
+    </div>
+    <div class="trustedby-box">
+        <img src="images/logo-northern-tier-high-adventure.png" alt="Northern Tier High Adventure, BSA Logo">
+        <p id="para1"><span>Northern Tier High Adventure</span><br>Boy Scouts of America <br>Ely, Minnesota</p>           
+    </div>
+    <div class="trustedby-box">
+        <img src="images/threefirescouncil_logo.png" alt="Three Fires Council, BSA Logo">
+        <p id="para1"><span>Three Fires Council</span><br>Boy Scouts of America <br>St. Charles, Illinois</p>           
+    </div>
+</section>
+       
+<div class="section-break">
+<p id="headline3">Seasonal Employers</p>
+</div>
+<section class="trustedby-container">
+    <div class="trustedby-box">
+        <img src="images/Bradford_Woods_Logo.png" alt="Northern Tier High Adventure, BSA Logo">
+        <p id="para1"><span>Bradford Woods</span><br>Indiana University<br>Martinsville, Indiana</p>           
+    </div>
+    <div class="trustedby-box">
+        <img src="images/philmont-colorlogo.jpg" alt="Northern Tier High Adventure, BSA Logo">
+        <p id="para1"><span>Philmont Scout Ranch</span><br>Boy Scouts of America <br>Cimarron, New Mexico</p>           
+    </div>
+</section>
+        
+</section>
+    <section class="skills-container">
+        <div class="skills-box"></div>
+    </section>
+    <section class="intro-container">
+        <div class="intro-box">
+           
+        </div>
+</section>
+    
+
+
+
+
+</body>
+<?phpd
+require 'assets/footer.php';
+?>
